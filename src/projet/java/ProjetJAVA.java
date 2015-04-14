d37349fef8fc8b5aa0bcb5097b9806f700d7a13f
julidon <tomberthaud@gmail.com>
2015-04-14 16:26:05 +0200

    ajout packages

--- src/projet/java/ProjetJAVA.java
+++ src/projet/java/ProjetJAVA.java
@@ -17,7 +17,6 @@ public class ProjetJAVA {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("coucou");
-        ///lkjgfdsqgsdjgkljkhgfds
     }
     
 }
